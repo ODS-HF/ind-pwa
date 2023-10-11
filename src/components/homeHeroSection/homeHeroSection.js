54dca87243d47c0680f00ea8621897e25db62172
karkey-ods <karthi@odstdio.com>
2023-10-12 03:29:42 +0530

    carouselAndViewbutton

--- src/components/homeHeroSection/homeHeroSection.js
+++ src/components/homeHeroSection/homeHeroSection.js
@@ -3,6 +3,9 @@ import '../../components/homeHeroSection/homeHeroSection.css'
 import arrowCircle from '../../assets/images/homeHeroSection/ArrowCircleUpRight.svg'
 import card from '../../assets/images/homeHeroSection/Rectangle.svg'
 import arrowRectangle from '../../assets/images/homeHeroSection/arrowRectangle.svg'
+import divider from '../../assets/images/homeHeroSection/Divider.svg'
+import arrowRectangleWhite from '../../assets/images/homeHeroSection/arrowRectangleWhite.svg'
+
 const homeHeroSection = () => {
 
   const data = {
@@ -31,7 +34,6 @@ const homeHeroSection = () => {
             </div>
 
             <div className='heroSection-body'>
-
                 <div className='card-container'>
                     <div className='your-card-button'>
                         
@@ -50,6 +52,33 @@ const homeHeroSection = () => {
                     </div>
                     <img src={card} className='card' alt='card'/>
                 </div>
+
+                <div className='divider-design'>
+                    <img src={divider} alt='divider'/>
+                </div>
+
+                <div className='carouselAndViewbutton'>
+                        <div className='carousel-card'>
+                            [ Place holder for carousel ]
+                        </div>
+
+                        <div className='view-all-container'>
+                            <div className='view-all-button'>
+                                    <div className='view-text'> 
+                                        view
+                                    </div>
+
+                                    <div className='all-text'>
+                                        all
+                                    </div>
+
+                                    <div className='arrowRectangleWhite'>
+                                        <img src={arrowRectangleWhite} alt='arr'/>
+                                    </div>
+                            </div>
+                        </div>
+                </div>
+                
             </div>
         </div>
     </>
